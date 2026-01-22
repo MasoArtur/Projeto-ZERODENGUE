dc2bd09db072cd48ae555996d5e2b7ad7b75cfc0
Artur Maso <masoartur@gmail.com>
2026-01-22 19:11:40 -0300

    Resolvendo e melhorando
    Implementando e testando outras coisas no Js

--- controller/cadastrar_controller.js
+++ controller/cadastrar_controller.js
@@ -1,12 +1,12 @@
 
-const objetoUsuario = [
-    {
-        nome: "",
-        telefone: "",
-        email: "",
-        senha_confirmada: "",
-    },
-]
+// const objetoUsuario = [
+//     {
+//         nome: "",
+//         telefone: "",
+//         email: "",
+//         senha_confirmada: "",
+//     },
+// ]
 const nome_usuario = document.getElementById('nome_usuario');
 
 const telefone_usuario = document.getElementById('telefone_usuario');
@@ -17,6 +17,15 @@ const senha_usuario = document.getElementById('senha');
 
 const confirmar_senha = document.getElementById('confirmar_senha');
 
+const icon_senha = document.getElementById('olho_senha1');
+const icon_confirmar_senha = document.getElementById("olho_senha2");
+
+icon_senha.addEventListener("click", mostrarSenha);
+icon_confirmar_senha.addEventListener("click", mostrarSenha);
+
+// testar com query selector para mudar o passwordview
+
+let passwordview = false;
 function criarUsuario(){
     if(!nome_usuario || !telefone_usuario || !email_usuario || !senha_usuario || !confirmar_senha){
         alert("preencha todos os campos");
@@ -27,4 +36,16 @@ if(senha_usuario.value == confirmar_senha.value){
     alert("")
 }else{
 
+}
+
+function mostrarSenha( ){
+    let escrever_senha = document.getElementsByClassName();
+
+    if(passwordview){
+        icon_senha.className = "fa-solid fa-eye";
+        icon_confirmar_senha.className = "fa-solid fa-eye";
+        escrever_senha.type = "password";
+    } else{
+
+    }
 }
--- controller/section_controller.js
+++ controller/section_controller.js
@@ -1,11 +1,18 @@
-document.getElementById("btn_campanha").addEventListener("click", mudarSection);
+// document.getElementById("btn_campanha").addEventListener("click", mudarSection);
 
-document.getElementById("btn_quem_somos2").addEventListener("click",voltarSection);
+// document.getElementById("btn_quem_somos").addEventListener("click", voltarSection);
 
-function mudarSection(){
-    document.getElementById("Tentativa").scrollIntoView({behavior: "smooth"});
-}
+// function mudarSection() {
+//     document.getElementById("Tentativa").scrollIntoView({ behavior: "smooth" });
+// }
 
-function voltarSection(){
-    document.getElementById("quem-somos").scrollIntoView({behavior: "smooth"});
+// function voltarSection() {
+//     document.getElementById("quem-somos").scrollIntoView({ behavior: "smooth" });
+// }
+
+//codigo felipetto ajudas
+const banners = document.getElementById("banners");
+
+function trocarBanner(numeroBanner) {
+    banners.style.top = `-${numeroBanner * 100}vh`;
 }
