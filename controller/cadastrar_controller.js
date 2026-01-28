d314949f54a0dba38ea7cec38207efda1d0cfd5b
Artur Maso <masoartur@gmail.com>
2026-01-28 16:29:11 -0300

    Dominando tudo

--- controller/cadastrar_controller.js
+++ controller/cadastrar_controller.js
@@ -18,10 +18,9 @@ const senha_usuario = document.getElementById('senha');
 const confirmar_senha = document.getElementById('confirmar_senha2');
 
 let btn_cadastrar = document.getElementById("btn_conta");
-btn_cadastrar.addEventListener("click", () => {
-    criarUsuario();
-})
-
+    btn_cadastrar.addEventListener("click", () => {
+        criarUsuario();
+});
 
 function criarUsuario(){
     if(!nome_usuario || !telefone_usuario || !email_usuario || !senha_usuario || !confirmar_senha){
@@ -29,65 +28,59 @@ function criarUsuario(){
     };
 
     if(senha_usuario.value === confirmar_senha.value){
-    alert("Senha correta, continue com o cadastro por favor!")
-    }else{
-    alert("A senha esta errada")
+        alert("Senha correta, continue com o cadastro por favor!")
+    } else{
+        alert("A confirmação da senha não coincide com a senha");
     };
 }
 
+//     const icon_senha = document.getElementById('olho_senha1');
+//     const icon_confirmar_senha = document.getElementById("olho_senha2");
+//     icon_senha.addEventListener("click", mostrarSenha);
+//     icon_confirmar_senha.addEventListener("click", mostrarSenha);
 
-    const icon_senha = document.getElementById('olho_senha1');
-    const icon_confirmar_senha = document.getElementById("olho_senha2");
-    icon_senha.addEventListener("click", mostrarSenha);
-    icon_confirmar_senha.addEventListener("click", mostrarSenha);
+// // testar com query selector para mudar o passwordview
+// let passwordview = false;
 
-// testar com query selector para mudar o passwordview
-let passwordview = false;
+// function mostrarSenha(){
+//     let escrever_senha = document.getElementById("senha");
+//     let escrever_confirmar_senha = document.getElementById("confirmar_senha2");
 
-function mostrarSenha(){
-    let escrever_senha = document.getElementById("senha");
-    let escrever_confirmar_senha = document.getElementById("confirmar_senha2");
+//     if(passwordview){
+//         icon_senha.className = "fa-solid fa-eye";
+//         icon_confirmar_senha.className = "fa-solid fa-eye";
+//         escrever_senha.type = "password";
+//         escrever_confirmar_senha.type = "password";
+//     } else{
+//         icon_senha.className = "fa-solid fa-eye-slash";
+//         icon_confirmar_senha.className = "fa-solid fa-eye-slash";
+//         escrever_senha.type = "text";
+//         escrever_confirmar_senha.type = "text";
+//     }
+//     passwordview = !passwordview;  
+// }
 
-    if(passwordview){
-        icon_senha.className = "fa-solid fa-eye";
-        icon_confirmar_senha.className = "fa-solid fa-eye";
-        escrever_senha.type = "password";
-        escrever_confirmar_senha.type = "password";
-    } else{
-        icon_senha.className = "fa-solid fa-eye-slash";
-        icon_confirmar_senha.className = "fa-solid fa-eye-slash";
-        escrever_senha.type = "text";
-        escrever_confirmar_senha.type = "text";
-    }
-    passwordview = !passwordview;  
-}
+// ideia que eu tive com o gpt
+let cadastrar_senha = document.getElementById("senha");
+let cadastrar_confirmar_senha = document.getElementById("confirmar_senha2");
 
+const icon_senha1 = document.getElementById("olho_senha1");
+const icon_confirmar_senha1 = document.getElementById("olho_senha2");
 
-//ideia que eu tive com o gpt
-
-/*const icon_senha = document.getElementById('olho_senha1');
-const icon_confirmar_senha = document.getElementById("olho_senha2");
-
-const senha_usuario = document.getElementById('senha');
-const confirmar_senha = document.getElementById('confirmar_senha2');
-
-icon_senha.addEventListener("click", () => {
-    mostrarSenha(senha_usuario, icon_senha);
+icon_senha1.addEventListener('click', () => {
+    mudarOlhoSenha(cadastrar_senha, icon_senha1);
 });
 
-icon_confirmar_senha.addEventListener("click", () => {
-    mostrarSenha(confirmar_senha, icon_confirmar_senha);
+icon_confirmar_senha1.addEventListener('click', () =>{
+    mudarOlhoSenha(cadastrar_confirmar_senha, icon_confirmar_senha1);
 });
 
-function mostrarSenha(input, icon){
-
-    if(input.type === "password"){
-        input.type = "text";
+function mudarOlhoSenha(inputID, icon){
+    if(inputID.type === "password"){
+        inputID.type = "text";
         icon.className = "fa-solid fa-eye-slash";
     } else{
-        input.type = "password";
+        inputID.type = "password";
         icon.className = "fa-solid fa-eye";
     }
-
-}
-*/
+};
